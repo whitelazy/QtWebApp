890b4f5ced6d345031e9a0767ff22ee5018cf845
Stefan Frings <stefan@stefanfrings.de>
2019-01-17 17:47:57 +0000

    First import

---
+++ Demo1/src/main.cpp
@@ -0,0 +1,138 @@
+/**
+  @file
+  @author Stefan Frings
+*/
+
+#include <QCoreApplication>
+#include <QDir>
+#include "httplistener.h"
+#include "templatecache.h"
+#include "httpsessionstore.h"
+#include "staticfilecontroller.h"
+#include "filelogger.h"
+#include "requestmapper.h"
+
+using namespace stefanfrings;
+
+/** Cache for template files */
+TemplateCache* templateCache;
+
+/** Storage for session cookies */
+HttpSessionStore* sessionStore;
+
+/** Controller for static files */
+StaticFileController* staticFileController;
+
+/** Redirects log messages to a file */
+FileLogger* logger;
+
+/** Search the configuration file */
+QString searchConfigFile()
+{
+    QString binDir=QCoreApplication::applicationDirPath();
+    QString appName=QCoreApplication::applicationName();
+    QString fileName(appName+".ini");
+
+    QStringList searchList;
+    searchList.append(binDir);
+    searchList.append(binDir+"/etc");
+    searchList.append(binDir+"/../etc");
+    searchList.append(binDir+"/../../etc"); // for development without shadow build
+    searchList.append(binDir+"/../"+appName+"/etc"); // for development with shadow build
+    searchList.append(binDir+"/../../"+appName+"/etc"); // for development with shadow build
+    searchList.append(binDir+"/../../../"+appName+"/etc"); // for development with shadow build
+    searchList.append(binDir+"/../../../../"+appName+"/etc"); // for development with shadow build
+    searchList.append(binDir+"/../../../../../"+appName+"/etc"); // for development with shadow build
+    searchList.append(QDir::rootPath()+"etc/opt");
+    searchList.append(QDir::rootPath()+"etc");
+
+    foreach (QString dir, searchList)
+    {
+        QFile file(dir+"/"+fileName);
+        if (file.exists())
+        {
+            // found
+            fileName=QDir(file.fileName()).canonicalPath();
+            qDebug("Using config file %s",qPrintable(fileName));
+            return fileName;
+        }
+    }
+
+    // not found
+    foreach (QString dir, searchList)
+    {
+        qWarning("%s/%s not found",qPrintable(dir),qPrintable(fileName));
+    }
+    qFatal("Cannot find config file %s",qPrintable(fileName));
+}
+
+
+/**
+  Entry point of the program.
+*/
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc,argv);
+
+    app.setApplicationName("Demo1");
+    app.setOrganizationName("Butterfly");
+
+    // Find the configuration file
+    QString configFileName=searchConfigFile();
+
+    // Configure logging into a file
+    /*
+    QSettings* logSettings=new QSettings(configFileName,QSettings::IniFormat,&app);
+    logSettings->beginGroup("logging");
+    FileLogger* logger=new FileLogger(logSettings,10000,&app);
+    logger->installMsgHandler();
+    */
+
+    // Configure template loader and cache
+    QSettings* templateSettings=new QSettings(configFileName,QSettings::IniFormat,&app);
+    templateSettings->beginGroup("templates");
+    templateCache=new TemplateCache(templateSettings,&app);
+
+    // Configure session store
+    QSettings* sessionSettings=new QSettings(configFileName,QSettings::IniFormat,&app);
+    sessionSettings->beginGroup("sessions");
+    sessionStore=new HttpSessionStore(sessionSettings,&app);
+
+    // Configure static file controller
+    QSettings* fileSettings=new QSettings(configFileName,QSettings::IniFormat,&app);
+    fileSettings->beginGroup("docroot");
+    staticFileController=new StaticFileController(fileSettings,&app);
+
+    // Configure and start the TCP listener
+    QSettings* listenerSettings=new QSettings(configFileName,QSettings::IniFormat,&app);
+    listenerSettings->beginGroup("listener");
+    new HttpListener(listenerSettings,new RequestMapper(&app),&app);
+
+    qWarning("Application has started");
+
+    qDebug("create session1");
+    HttpSession session1(true);
+    session1.set("name","foo1");
+
+    qDebug("create session2");
+    HttpSession session2(true);
+    session2.set("name","foo2");
+
+    qDebug("create session3");
+    HttpSession session3(true);
+    session3.set("name","foo3");
+
+    qDebug("copy session 1 to session 3");
+    session3=session1;
+
+    qDebug("copy session 2 to session 3");
+    session3=session2;
+
+    qDebug("copy session 3 to session 3");
+    session3=session3;
+    qDebug("name in session3 %s is %s",session3.getId().constData(),session3.get("name").toByteArray().data());
+
+    // app.exec();
+
+    qWarning("Application has stopped");
+}
